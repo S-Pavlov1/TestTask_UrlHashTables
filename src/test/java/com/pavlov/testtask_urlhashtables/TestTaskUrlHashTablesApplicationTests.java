735af968fb790d9362822e56e3ce58be584e926a
S.Pavlov <s.pavlov1@g.nsu.ru>
2022-06-14 14:21:54 +0700

    init commit

---
+++ src/test/java/com/pavlov/testtask_urlhashtables/TestTaskUrlHashTablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavlov.testtask_urlhashtables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskUrlHashTablesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
